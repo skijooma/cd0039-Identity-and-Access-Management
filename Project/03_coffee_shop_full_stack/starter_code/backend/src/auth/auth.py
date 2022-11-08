1d1a651072ffa327f5382266f3956eb07135599c
Steven Kijooma <skijooma@andrew.cmu.edu>
2022-11-08 23:03:56 +0200

    feat: Auth0 setup & get_token_auth_header()

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -1,14 +1,18 @@
 from functools import wraps
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+from flask import abort, request
+
+AUTH0_DOMAIN = 'dev-4ezltnbcex7uvmp5.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'ffsnd'
 
 ## AuthError Exception
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
 '''
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
@@ -25,8 +29,46 @@ class AuthError(Exception):
         it should raise an AuthError if the header is malformed
     return the token part of the header
 '''
+
+
 def get_token_auth_header():
-   raise Exception('Not Implemented')
+    """
+    Retrieves the Access Token from the Authorization Header.
+    """
+
+    # Checking if authorization header is not present in the request.
+    if 'Authorization' not in request.headers:
+        raise AuthError("Authorization missing", 401)
+
+    auth_header = request.headers['Authorization']
+
+    parts = auth_header.split()
+
+    # Checking if bearer part is missing.
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'Malformed header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    # Checking if header is missing a part.
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'Invalid header',
+            'description': 'Header incomplete.'
+        }, 401)
+
+    # Checking if header has excess parts.
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'Invalid header',
+            'description': 'Authorization header has excess parts.'
+        }, 401)
+
+    token = parts[1]
+
+    return token
+
 
 '''
 @TODO implement check_permissions(permission, payload) method
@@ -39,8 +81,11 @@ def get_token_auth_header():
     it should raise an AuthError if the requested permission string is not in the payload permissions array
     return true otherwise
 '''
+
+
 def check_permissions(permission, payload):
     raise Exception('Not Implemented')
+
 
 '''
 @TODO implement verify_decode_jwt(token) method
@@ -55,8 +100,11 @@ def check_permissions(permission, payload):
 
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
+
+
 def verify_decode_jwt(token):
     raise Exception('Not Implemented')
+
 
 '''
 @TODO implement @requires_auth(permission) decorator method
@@ -68,6 +116,8 @@ def verify_decode_jwt(token):
     it should use the check_permissions method validate claims and check the requested permission
     return the decorator which passes the decoded payload to the decorated method
 '''
+
+
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
@@ -78,4 +128,5 @@ def requires_auth(permission=''):
             return f(payload, *args, **kwargs)
 
         return wrapper
+
     return requires_auth_decorator
