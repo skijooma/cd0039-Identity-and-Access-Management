c38708f238e20d9fdfd1092eaaa1fc038cba91ea
Steven Kijooma <skijooma@andrew.cmu.edu>
2022-11-17 01:12:41 +0200

    envs: postman_collection.json & frontend - environement.ts

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'https://dev-4ezltnbcex7uvmp5.us.auth0.com/authorize?', // the auth0 domain prefix
+    audience: 'ffsnd', // the audience set for the auth0 app
+    clientId: 'V2cmp8AICx4yXN3pfTNnT3prnjhWWx89&', // the client id generated for the auth0 app
+    callbackURL: 'https://localhost:8080/callback', // the base url of the running ionic application.
   }
 };
